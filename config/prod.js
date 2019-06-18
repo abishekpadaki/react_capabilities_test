537c5b9613ac8a25073f616e15a4164facc23173
Sammed Ravanavar <33900745+sammedravanavar@users.noreply.github.com>
2019-06-18 15:34:54 +0530

    es6 in webpack config

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,40 @@
-const webpack = require("webpack");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = function(configDirs) {
+module.exports = configDirs => {
   let prodConfig = Object.assign({}, require("./common")(configDirs));
   prodConfig = {
     ...prodConfig,
-    optimization: { minimizer: [new UglifyJsPlugin()] }
+    module: {
+      ...prodConfig.module,
+      rules: [
+        ...prodConfig.module.rules,
+        {
+          test: /\.(css|scss)$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
+            "css-loader",
+            "sass-loader"
+          ]
+        }
+      ]
+    },
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({ parallel: true }),
+        new MiniCssExtractPlugin(),
+        new OptimizeCssAssetsPlugin({
+          cssProcessorPluginOptions: {
+            preset: ["default", { discardComments: { removeAll: true } }]
+          }
+        })
+      ],
+      splitChunks: { chunks: "all" }
+      // runtimeChunk: true
+    }
   };
   return prodConfig;
 };
