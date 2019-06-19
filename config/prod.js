03916fa8148d1ec20fb53ae8cb4e4c97c8c5badd
Sammed Ravanavar <33900745+sammedravanavar@users.noreply.github.com>
2019-06-19 09:43:27 +0530

    separate bundles for modules

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,17 @@
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const MinifyPlugin = require("babel-minify-webpack-plugin");
 
 module.exports = configDirs => {
   let prodConfig = Object.assign({}, require("./common")(configDirs));
   prodConfig = {
     ...prodConfig,
+    devtool: "source-map",
+    output: {
+      ...prodConfig.output,
+      filename: "[name].[contenthash:8].js"
+    },
     module: {
       ...prodConfig.module,
       rules: [
@@ -22,18 +28,41 @@ module.exports = configDirs => {
         }
       ]
     },
+    plugins: [
+      ...prodConfig.plugins,
+      // new MinifyPlugin(),
+      new MiniCssExtractPlugin({ filename: "[name].css" })
+    ],
     optimization: {
       minimizer: [
-        new UglifyJsPlugin({ parallel: true }),
-        new MiniCssExtractPlugin(),
+        new TerserPlugin({ parallel: true }),
         new OptimizeCssAssetsPlugin({
           cssProcessorPluginOptions: {
             preset: ["default", { discardComments: { removeAll: true } }]
           }
         })
       ],
-      splitChunks: { chunks: "all" }
-      // runtimeChunk: true
+      splitChunks: {
+        chunks: "all",
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+              return `npm.${packageName.replace("@", "")}`;
+            }
+          },
+          styles: {
+            name: "styles",
+            test: /\.css$/,
+            chunks: "all",
+            enforce: true
+          }
+        }
+      },
+      runtimeChunk: "single"
     }
   };
   return prodConfig;
